5921b859cfb3620319c3a288a496e95cc1d89c90
soleyman79 <59196723+soleyman79@users.noreply.github.com>
2022-07-12 10:47:24 +0430

    insert other punctuation cleanup

--- eliza.py
+++ eliza.py
@@ -175,7 +175,10 @@ class Eliza:
         text = re.sub(r'\s*\.+\s*', ' . ', text)
         text = re.sub(r'\s*,+\s*', ' , ', text)
         text = re.sub(r'\s*;+\s*', ' ; ', text)
-        # TODO: insert other punctuation like clone
+        text = re.sub(r'\s*:+\s*', ' : ', text)
+        text = re.sub(r'\s*،+\s*', ' ، ', text)
+        text = re.sub(r'\s*؛+\s*', ' ؛ ', text)
+        # TODO: insert other punctuation cleanup
         log.debug('After punctuation cleanup: %s', text)
 
         words = [w for w in text.split(' ') if w]
